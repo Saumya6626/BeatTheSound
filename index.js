fa46218314a7b1d72ae31ef42faaf472792c89a4
Saumya6626 <saumya6626@gmail.com>
2021-06-17 06:55:35 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+ var a=document.querySelectorAll(".drum").length;
+
+ for(var i=0;i<a;i++){
+     document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+        var buttoninner=this.innerHTML;
+     makesound(buttoninner);
+      buttonanimation(buttoninner);
+         
+    });
+ }
+
+document.addEventListener("keydown",function(event){
+         makesound(event.key);
+         buttonanimation(event.key);
+});
+
+
+function makesound(key){
+     
+
+    switch (key) {
+        case "w":var sound=new Audio("sounds/crash.mp3");
+                     sound.play();
+                     break;
+        case "a":var sound=new Audio("sounds/kick-bass.mp3");
+                     sound.play();
+                     break;
+        case "s":var sound=new Audio("sounds/snare.mp3");
+                     sound.play();
+                     break;
+        case "d":var sound=new Audio("sounds/tom-1.mp3");
+                     sound.play();
+                     break;
+        case "j":var sound=new Audio("sounds/tom-2.mp3");
+                     sound.play();
+                     break;
+        case "k":var sound=new Audio("sounds/tom-3.mp3");
+                     sound.play();
+                     break;
+        case "l":var sound=new Audio("sounds/tom-4.mp3");
+                     sound.play();
+                     break;             
+                
+          default:
+              break;
+      }
+}
+function buttonanimation(currkey){
+    var activeButton=document.querySelector("."+currkey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    } ,100);
+}
+
